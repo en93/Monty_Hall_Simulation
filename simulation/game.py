be0cbc40aeab374e417cb26a4baead344f607e5b
ianba <ianbabington93@gmail.com>
2018-03-12 23:38:12 +1300

    New stuff

--- simulation/game.py
+++ simulation/game.py
@@ -1,19 +1,40 @@
 # Represents a single play of the game that is won or lost
 import random
+# from enum import Enum
+
+
 from simulation.door import Door, DoorStatus
 
 
 class Game:
 
-    DOOR_COUNT = 3
+    def __init__(self, strategy):
+        self.strategy = strategy
 
-    #Setup doors with randomly placed prize
-    doors = [Door()] * DOOR_COUNT
-    index_of_prize = random.randint(0, DOOR_COUNT-1)
-    doors[index_of_prize].value = DoorStatus.PRIZE #todo is there a more pythonic way that allows encapsulation?
+    def run(self):
 
-    #Make random choice from range of doors
-    index_of_door_chosen = random.randint(0, DOOR_COUNT-1)
-    door_chosen = doors[index_of_door_chosen]
+        DOOR_COUNT = 3 #todo make changable
 
-    #Open random door for player, remove door from list
+        #Setup doors with randomly placed prize
+        doors_all = [Door()] * DOOR_COUNT
+        index_of_prize = random.randint(0, DOOR_COUNT-1)
+        doors_all[index_of_prize].value = DoorStatus.PRIZE
+        index_door_chosen = random.randint(0, DOOR_COUNT - 1)
+
+        #Create data structures
+        door_chosen = doors_all[index_door_chosen]
+        door_prize = doors_all[index_of_prize]
+        doors_empty = filter(lambda x: x.value == DoorStatus.EMPTY, doors_all)
+
+        #Open random door for player, remove door from list
+
+
+        # if self.strategy
+
+
+
+class GameStrategy:
+    change = 0
+    keep = 1
+
+
