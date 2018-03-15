fdc16136f77590557b009e609a52230beba9aaea
ianba <ianbabington93@gmail.com>
2018-03-16 00:10:19 +1300

    New stuff

--- simulation/game.py
+++ simulation/game.py
@@ -1,40 +1,38 @@
 # Represents a single play of the game that is won or lost
 import random
-# from enum import Enum
 
 
 from simulation.door import Door, DoorStatus
 
 
 class Game:
+    DOOR_COUNT = 3  # todo make changable
 
     def __init__(self, strategy):
         self.strategy = strategy
 
-    def run(self):
+    def run(self):  # todo fix bug always wins
 
-        DOOR_COUNT = 3 #todo make changable
-
-        #Setup doors with randomly placed prize
-        doors_all = [Door()] * DOOR_COUNT
-        index_of_prize = random.randint(0, DOOR_COUNT-1)
+        # Setup doors with randomly placed prize
+        doors_all = [Door()] * self.DOOR_COUNT
+        index_of_prize = random.randint(0, self.DOOR_COUNT - 1)
         doors_all[index_of_prize].value = DoorStatus.PRIZE
-        index_door_chosen = random.randint(0, DOOR_COUNT - 1)
+        index_door_chosen = random.randint(0, self.DOOR_COUNT - 1)
 
-        #Create data structures
+        # Create data structures
         door_chosen = doors_all[index_door_chosen]
         door_prize = doors_all[index_of_prize]
-        doors_empty = filter(lambda x: x.value == DoorStatus.EMPTY, doors_all)
+        doors_closable = filter(lambda x: x.value == DoorStatus.EMPTY and x != door_prize, doors_all)
 
-        #Open random door for player, remove door from list
+        # Open random door for player, remove door from list
+        index_close = random.randint(0, len(doors_closable) - 1)  # todo consider 0 len case
+        doors_closable.pop(index_close)
 
+        # Use keep or change strategy, for now assume we are keeping
 
-        # if self.strategy
+        # Big reveal
+        return door_chosen == door_prize
 
-
-
-class GameStrategy:
-    change = 0
-    keep = 1
-
-
+    class GameStrategy:
+        change = 0
+        keep = 1
--- simulation/run_handler.py
+++ simulation/run_handler.py
@@ -1,5 +1,6 @@
 # Entry point for program execution, handles user input and starts simulations
 import sys
+from simulation.game import Game
 
 
 def main():
@@ -8,16 +9,17 @@ def main():
     number_of_runs = get_run_count()
     # todo is there a more elegant way of looping?
     for x in range(0, number_of_runs):
-        result = Game()
-        wins += result[0]
-        losses += result[1]
-    print(wins, losses)
-
+        result = Game(Game.GameStrategy.keep)
+        if result: #todo bad name there
+            wins += 1
+        else:
+            losses += 1
+    print("wins: ", wins, ", losses: ", losses)
 
 
 def get_run_count():
     if len(sys.argv) > 1:
-        num = sys.argv[1]
+        num = int(sys.argv[1])
     else:
         num = 3
     return num
